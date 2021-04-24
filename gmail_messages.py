fb1b2d070e280d4a53656133dbe6b84a8d0da3b5
pdmxdd <pdmxdd@gmail.com>
2021-04-24 13:40:52 -0500

    removed deprecated comments & import from gmail_messages.py

--- gmail_messages.py
+++ gmail_messages.py
@@ -1,13 +1,10 @@
 import base64
-from gmail_service import get_service
 
 def get_message_ids_by_query(query_string, service):
     """
     Returns a list of message ids that match a given query string.
     """
-    # service = get_service()
     message_results = service.users().messages().list(userId='me', q=query_string).execute()
-    # print("message_results: {}".format(message_results))
     if(message_results['resultSizeEstimate'] == 0):
         return []
     return message_results['messages'] if message_results['messages'] is not None else []
@@ -16,7 +13,6 @@ def get_message(message_id, service):
     """
     Returns a dictionary representation of a message from the Gmail API.
     """
-    # service = get_service()
     message = service.users().messages().get(userId='me', id=message_id).execute()
     return message
 
@@ -31,7 +27,6 @@ def trim_headers(all_headers, relevant_headers=["From", "To", "Subject", "Date"]
     Returns a trimmed dictionary representation of an emails headers.
     """
     data = {}
-    # print("all headers: {}".format(headers))
     for header in all_headers:
         if header['name'] in relevant_headers:
             data[header['name']] = header['value']
