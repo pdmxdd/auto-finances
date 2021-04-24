196f28b052b54319d42ef207d62c84064396118d
pdmxdd <pdmxdd@gmail.com>
2021-04-24 11:37:12 -0500

    updated gmail_messages to take a service instead of creating a new one each time

--- gmail_messages.py
+++ gmail_messages.py
@@ -1,22 +1,22 @@
 import base64
 from gmail_service import get_service
 
-def get_message_ids_by_query(query_string):
+def get_message_ids_by_query(query_string, service):
     """
     Returns a list of message ids that match a given query string.
     """
-    service = get_service()
+    # service = get_service()
     message_results = service.users().messages().list(userId='me', q=query_string).execute()
     # print("message_results: {}".format(message_results))
     if(message_results['resultSizeEstimate'] == 0):
         return []
     return message_results['messages'] if message_results['messages'] is not None else []
 
-def get_message(message_id):
+def get_message(message_id, service):
     """
     Returns a dictionary representation of a message from the Gmail API.
     """
-    service = get_service()
+    # service = get_service()
     message = service.users().messages().get(userId='me', id=message_id).execute()
     return message
 
--- tests.py
+++ tests.py
@@ -44,35 +44,35 @@ class MessageTests(unittest.TestCase):
 
     def test_get_message_id_by_query_from(self):
         query_string = 'from:"paul@paulmatthews.dev"'
-        message_ids = get_message_ids_by_query(query_string)
+        message_ids = get_message_ids_by_query(query_string, self.service)
         self.assertGreater(len(message_ids), 0)
         self.assertIn("id", message_ids[0])
         self.assertIn("threadId", message_ids[0])
 
     def test_get_message_id_by_query_label_recorded(self):
         query_string = 'from:"paul@paulmatthews.dev" label:"Auto-Finances/Recorded'
-        message_ids = get_message_ids_by_query(query_string)
+        message_ids = get_message_ids_by_query(query_string, self.service)
         self.assertGreater(len(message_ids), 0)
         self.assertIn("id", message_ids[0])
         self.assertIn("threadId", message_ids[0])
 
     def test_get_message_id_by_query_label_chase(self):
         query_string = 'from:"paul@paulmatthews.dev" label:"Auto-Finances/Transaction/Chase'
-        message_ids = get_message_ids_by_query(query_string)
+        message_ids = get_message_ids_by_query(query_string, self.service)
         self.assertGreater(len(message_ids), 0)
         self.assertIn("id", message_ids[0])
         self.assertIn("threadId", message_ids[0])
 
     def test_get_message_id_by_query_label_discover(self):
         query_string = 'from:"paul@paulmatthews.dev" label:"Auto-Finances/Transaction/Discover'
-        message_ids = get_message_ids_by_query(query_string)
+        message_ids = get_message_ids_by_query(query_string, self.service)
         self.assertGreater(len(message_ids), 0)
         self.assertIn("id", message_ids[0])
         self.assertIn("threadId", message_ids[0])
 
     def test_get_message_id_by_query_subject(self):
         query_string = 'from:"paul@paulmatthews.dev" subject:"Test Subject"'
-        message_ids = get_message_ids_by_query(query_string)
+        message_ids = get_message_ids_by_query(query_string, self.service)
         self.assertGreater(len(message_ids), 0)
         self.assertIn("id", message_ids[0])
         self.assertIn("threadId", message_ids[0])
