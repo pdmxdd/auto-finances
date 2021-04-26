2212fbbcd3a70b0fddf4627808611c201df8dcdb
pdmxdd <pdmxdd@gmail.com>
2021-04-26 00:51:14 -0500

    updated main so that it writes the message dicts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from csv_utils import delete_file, write_dict_list
 from chase_transactions import chase_message_to_dict
 from gmail_messages import get_message, get_message_ids_by_query
 from gmail_service import get_service
@@ -10,9 +11,13 @@ if __name__ == '__main__':
     service = get_service()
     labels_dict = get_labels_dict(service)
     
+    csv_filepath = './expenses.csv'
+
+    # for manual testing purposes only:
+    delete_file(csv_filepath)
+
     chase_messages = [get_message(message_id['id'], service) for message_id in get_message_ids_by_query('from:"paul@paulmatthews.dev" subject:"Your Single Transaction Alert From Chase"', service)]
 
-    for message in chase_messages:
-        print("chase_message_dict: {}".format(chase_message_to_dict(message)))
+    write_dict_list(csv_filepath, [chase_message_to_dict(message) for message in chase_messages])
 
-        # TODO: update chase message labels (remove INBOX & UNREAD; add Auto-Finances/Transaction/Chase & Auto-Finances/Recorded)
+    # TODO: update chase message labels (remove INBOX & UNREAD; add Auto-Finances/Transaction/Chase & Auto-Finances/Recorded) -> after a message is recorded it needs to have labels added and removed. will need to chop up the list comprehensions
