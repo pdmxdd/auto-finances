f6b4a8cb8bc3d916e3988ea71668819c4b680a59
pdmxdd <pdmxdd@gmail.com>
2021-04-29 00:47:18 -0500

    full suite of tests pass

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest
-# from tests.label import LabelTests
-# from tests.message_id import GetMessageIdTests
-# from tests.message import GetMessageTests
-# from tests.transactions_chase import ChaseTransactionsTests
-# from tests.write_transactions import WriteTransactionTests
-# from tests.message_labels import MessageRemoveLabelTests
+from tests.label import LabelTests
+from tests.message_id import GetMessageIdTests
+from tests.message import GetMessageTests
+from tests.transactions_chase import ChaseTransactionsTests
+from tests.write_transactions import WriteTransactionTests
+from tests.message_labels import MessageRemoveLabelTests
 from tests.transactions_discover import DiscoverTransactionsTests
 
 if __name__ == "__main__":
